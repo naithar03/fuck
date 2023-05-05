c4578f63b889034fa19f67b5e9e6a408d778afd7
naithar03 <130193760+naithar03@users.noreply.github.com>
2023-05-05 07:42:49 +0000

    23-05-05 191 ~ 192 pg

---
+++ .ipynb_checkpoints/list-checkpoint.py
@@ -0,0 +1,31 @@
+# # 테스트 입니다. 코드 스페이스 2023-04-22 / 17:15
+# # ipynb 는 주피터 확장자 입니다.
+
+# import sys
+
+# print(sys.version)
+
+# orgPWd = int(input("id로 사용할 ID 입력 >> "))
+
+# keyMa = 123456
+
+# print(orgPWd ^ keyMa)
+
+# # if 문 예시
+# PM = 93
+
+# if 151 <= PM:
+#     print("... 등급 매우 나쁨")
+# elif 81 <= PM:
+#     print("... 등급 나쁨")
+# elif 31 <= PM:
+#     print("... 등급 보통")
+# else:
+#     print("... 등급 좋음")
+
+
+# 2023-05-05
+
+# %%
+p = 'python'
+y = 'python'
--- list.py
+++ list.py
@@ -1,24 +1,66 @@
-# 테스트 입니다. 코드 스페이스 2023-04-22 / 17:15
-# ipynb 는 주피터 확장자 입니다.
+# # 테스트 입니다. 코드 스페이스 2023-04-22 / 17:15
+# # ipynb 는 주피터 확장자 입니다.
 
-import sys
+# import sys
 
-print(sys.version)
+# print(sys.version)
 
-orgPWd = int(input("id로 사용할 ID 입력 >> "))
+# orgPWd = int(input("id로 사용할 ID 입력 >> "))
 
-keyMa = 123456
+# keyMa = 123456
 
-print(orgPWd ^ keyMa)
+# print(orgPWd ^ keyMa)
 
-# if 문 예시
-PM = 93
+# # if 문 예시
+# PM = 93
 
-if 151 <= PM:
-    print("... 등급 매우 나쁨")
-elif 81 <= PM:
-    print("... 등급 나쁨")
-elif 31 <= PM:
-    print("... 등급 보통")
-else:
-    print("... 등급 좋음")
+# if 151 <= PM:
+#     print("... 등급 매우 나쁨")
+# elif 81 <= PM:
+#     print("... 등급 나쁨")
+# elif 31 <= PM:
+#     print("... 등급 보통")
+# else:
+#     print("... 등급 좋음")
+
+
+# 2023-05-05
+
+# %%
+p = 'python'
+y = "python"
+
+print(p,y)
+
+## immutanle 문자열, 정수, 실수
+# p[0] = 'a' 'p' does not support item assignmentpylint(unsupported-assignment-operation)
+
+print(id(p), id(y))
+print(p is y)
+print(type(p), type(y))
+
+# %%
+n = 1
+print(type(n))
+m = 1, # 콤마 붙으면 튜플 // m = (1, ) <== 이거와 같은 의미임
+print(type(m))
+
+# %%
+t1 = (1, 2, 3)
+t2 = (1, 2, 3)
+t1 == t2
+# %%
+t2 = (3, 2, 1)
+t1 == t2
+# %%
+print(id(t1), id(t2))
+t1 is t2
+
+# %% 
+t1 = (1, 2, 3)
+# t1[0] = 1
+lis = [1, 2, 3]
+lis[2] = 20
+lis
+# 191 ~ 192 페이지 내용
+# %%
